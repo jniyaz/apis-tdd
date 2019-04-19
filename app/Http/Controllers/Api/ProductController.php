9e38f3bab5a3b288ec7efa62aff4a1c5652ea6ee
Niyaz <niyaz@jocom.my>
2019-04-19 16:11:58 +0800

    Product create test with api routes

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProductController extends Controller
+{
+    public function store(Request $request) {
+
+        $product = Product::create([
+            'name' => $request->name,
+            'slug' => str_slug($request->slug),
+            'price' => $request->price
+        ]);
+
+        return response()->json($product, 201);
+    }
+}
--- tests/Feature/Http/Controllers/Api/ProductControllerTest.php
+++ tests/Feature/Http/Controllers/Api/ProductControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers\Api;
 
+use Faker\Factory;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -13,6 +14,35 @@ class ProductControllerTest extends TestCase
      */
     public function can_create_a_product()
     {
-        $this->assertTrue(true);
+        $faker = Factory::create();
+        
+        // Given
+            // user is authenticated (..will come back to this test)
+        
+        // When
+            // POST request to create a product
+            $response = $this->json('POST', 'api/products', [
+                'name' => $name = $faker->company,
+                'slug' => str_slug($name),
+                'price' => $price = random_int(10, 100)
+            ]);
+        
+        // Then 
+            // product exists
+            $response
+                    ->assertJsonStructure(['id', 'name', 'price', 'created_at']) // exactly it shoud return like this otherwise fails
+                    ->assertJson([
+                        'name' => $name,
+                        'slug' => str_slug($name),
+                        'price' => $price
+                    ])  // exactly requested data to be return otherwise fails
+                    ->assertStatus(201);
+            
+            $this
+                ->assertDatabaseHas('products', [
+                    'name' => $name,
+                    'slug' => str_slug($name),
+                    'price' => $price
+                ]);
     }
 }
