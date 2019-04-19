78c5db0ab03c534f6539d0669427a4d004cab0e3
Niyaz <niyaz@jocom.my>
2019-04-19 15:21:27 +0800

    Initial Commit

---
+++ tests/Feature/Http/Controllers/Api/ProductControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductControllerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function can_create_a_product()
+    {
+        $this->assertTrue(true);
+    }
+}
