41d10e5cb6ef20604dc07a699d93e2e70fe0867c
Niyaz <jniyaz@gmail.com>
2020-01-21 20:16:55 +0800

    auth implementation and checout feature test implementation

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Library - TDD
+// Library Application - using TDD approach
 
 // Book
 Route::post('/books', 'BooksController@store');
 Route::patch('/books/{book}-{slug}', 'BooksController@update');
 Route::delete('/books/{book}-{slug}', 'BooksController@destroy');
+
 // Author
 Route::post('/author', 'AuthorsController@store');
+
+// book checkout
+Route::post('/checkout/{book}', 'BookCheckoutController@store');
+Route::post('/checkin/{book}', 'BookCheckInController@store');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
